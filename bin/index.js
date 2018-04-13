39f100320e77101982a7fa1e08a2b29211cca5b1
Jaime Flores <jaimelopesflores@gmail.com>
2018-04-12 23:39:48 -0300

    managing flowers

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,32 @@
 
-const { events, read } = require('../lib/manager');
+const { events, init, manage } = require('../lib/manager');
 
 let designSpecs;
 
 events.on('onDesignSpecs', (specs) => {
 
-  console.log(specs);
   designSpecs = specs;
 });
 
-events.on('onFlower', (flower) => {
-
-  console.log(flower);
-});
 
 events.on('onFinish', () => {
 
   console.log('finish');
+
+  designSpecs.map((spec) => {
+
+    console.log('>>>>>>>>>>>>>');
+    console.log(spec.spec);
+    console.log(spec.totalAssigned());
+    spec.flowers.map((flower) => {
+      console.log('***');
+      console.log(`name :${flower.name}`);
+      console.log(`assigned :${flower.assigned}`);
+      console.log(`quantity :${flower.quantity}`);
+      console.log(`not in spec :${flower.notInSpec}`);
+    })
+  });
+
 });
 
-read(process.argv[2]);
+init(process.argv[2]);
--- tests/design-spec-unit-test.js
+++ tests/design-spec-unit-test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
-const { parse } = require('../lib/design-spec-parser');
+const { parse, parseFlower } = require('../lib/design-spec');
 
-test('should parse an design specification', (assert) => {
+test('should parse a design specification', (assert) => {
 
   let bouquetSpec = parse('AL10r5t8d30');
 
@@ -17,6 +17,29 @@ test('should parse an design specification', (assert) => {
 
   assert.deepEqual(total, 23, 'should be 23');
   assert.ok(total <= bouquetSpec.max, 'should be less or equal the maximum allowed');
+
+  bouquetSpec = parse('ab10r5t8d30');
+
+  // testing the design specifications
+  assert.notOk(/[A-Z]/.test(bouquetSpec.design), 'should not be a small letter');
+  assert.notOk(/S|L/.test(bouquetSpec.size), 'should be the capital letter S or L');
+
+  assert.end();
+});
+
+test('should parse a flower', (assert) => {
+
+  let flowerSpec = parseFlower('sL');
+
+  // testing the flower specifications
+  assert.ok(/[a-z]/.test(flowerSpec.name), 'should be a small letter');
+  assert.ok(/S|L/.test(flowerSpec.size), 'should be the capital letter S or L');
+
+  flowerSpec = parse('sA');
+
+  // testing the flowerspecifications
+  assert.notOk(/[a-a]/.test(flowerSpec.design), 'should not be a small letter');
+  assert.notOk(/S|L/.test(flowerSpec.size), 'should be the capital letter S or L');
 
   assert.end();
 });
