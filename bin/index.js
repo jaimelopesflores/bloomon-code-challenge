b66d2be488775afa777edf039c8f9e204068b748
Jaime Flores <jaimelopesflores@gmail.com>
2018-04-12 21:38:30 -0300

    reading the input file

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,22 @@
-const program = require('commander');
 
+const { events, read } = require('../lib/manager');
+
+let designSpecs;
+
+events.on('onDesignSpecs', (specs) => {
+
+  console.log(specs);
+  designSpecs = specs;
+});
+
+events.on('onFlower', (flower) => {
+
+  console.log(flower);
+});
+
+events.on('onFinish', () => {
+
+  console.log('finish');
+});
+
+read(process.argv[2]);
--- tests/design-spec-unit-test.js
+++ tests/design-spec-unit-test.js
@@ -1,21 +1,22 @@
 const test = require('tape');
-const { parse } = require('../lib/design-spec');
+const { parse } = require('../lib/design-spec-parser');
 
 test('should parse an design specification', (assert) => {
 
-  let bouquetSpec = parse('');
+  let bouquetSpec = parse('AL10r5t8d30');
 
   // testing the design specifications
   assert.ok(/[A-Z]/.test(bouquetSpec.design), 'should be a capital letter');
   assert.ok(/S|L/.test(bouquetSpec.size), 'should be the capital letter S or L');
 
   // testing the flower names
-  bouquetSpec.flowers.forEach((flower) => assert.ok(/[a-z]/.test(flower.name), 'should be a smal letter'));
+  bouquetSpec.flowers.forEach((flower) => assert.ok(/[a-z]/.test(flower.name), 'should be a small letter'));
 
-  let total = bouquetSpec.flowers.reduce((acc, current, index) => (index === 0 ? acc.quantity : acc) + curr.quantity);
+  // testing the total count
+  let total = bouquetSpec.flowers.reduce((acc, current, index) => (index === 1 ? acc.quantity : acc) + current.quantity);
 
+  assert.deepEqual(total, 23, 'should be 23');
   assert.ok(total <= bouquetSpec.max, 'should be less or equal the maximum allowed');
 
   assert.end();
-
 });
