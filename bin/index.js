54815f093f5f132ad801924e93a9173a3f4c4e10
Jaime Flores <jaimelopesflores@gmail.com>
2018-04-12 23:42:27 -0300

    adding arguments validation

--- bin/index.js
+++ bin/index.js
@@ -1,32 +1,9 @@
 
-const { events, init, manage } = require('../lib/manager');
+const { init, manage } = require('../lib/manager');
 
-let designSpecs;
-
-events.on('onDesignSpecs', (specs) => {
-
-  designSpecs = specs;
-});
-
-
-events.on('onFinish', () => {
-
-  console.log('finish');
-
-  designSpecs.map((spec) => {
-
-    console.log('>>>>>>>>>>>>>');
-    console.log(spec.spec);
-    console.log(spec.totalAssigned());
-    spec.flowers.map((flower) => {
-      console.log('***');
-      console.log(`name :${flower.name}`);
-      console.log(`assigned :${flower.assigned}`);
-      console.log(`quantity :${flower.quantity}`);
-      console.log(`not in spec :${flower.notInSpec}`);
-    })
-  });
-
-});
+if (process.argv.length !== 3) {
+  console.error('This tool expect 1 argument');
+  return process.exit(1);
+}
 
 init(process.argv[2]);
