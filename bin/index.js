ef88ef651d5064295f6cd3d5587762e5001ac059
Jaime Flores <jaimelopesflores@gmail.com>
2018-04-13 00:05:29 -0300

    preparing as a command line

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
 
 const { init, manage } = require('../lib/manager');
+const [,, ...args] = process.argv;
 
-if (process.argv.length !== 3) {
-  console.error('This tool expect 1 argument');
+if (args.length !== 1) {
+  console.error('[error] This tool expect 1 argument');
   return process.exit(1);
 }
 
